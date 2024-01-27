c114c590a1ddf54864d684773405d03fe87d8737
HLee <lhslhg@gmail.com>
2024-01-28 07:28:32 +0900

    add singletone scenemanager

--- Scripts/Controller/SceneManager.cs
+++ Scripts/Controller/SceneManager.cs
@@ -3,14 +3,34 @@ using System;
 
 public partial class SceneManager : Node
 {
+	[Export]
+	PackedScene StageSelectScene;
+
+	[Export]
+	PackedScene MainScene;
+
+	[Export]
+	string[] MapPaths = new string[6];
+
+	string _MapPath;
+
+	public string MapPath { get =>_MapPath;  }
+
+	public void LoadStage(int stage)
+	{
+		_MapPath = MapPaths[stage];
+		GetTree().ChangeSceneToPacked(MainScene);
+		
+	}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        var button = new Button();
-        button.Text = "Game Start";
-        button.Pressed += StartToStageSelect;
-        AddChild(button);
-    }
+		_MapPath = MapPaths[0];
+		var button = new Button();
+		button.Text = "Game Start";
+		button.Pressed += StartToStageSelect;
+		AddChild(button);
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -19,8 +39,8 @@ public partial class SceneManager : Node
 
 	public void StartToStageSelect()
 	{
-        GetTree().ChangeSceneToFile("res://Scenes/StageSelect/StageSelect.tscn"); 
-    }
+		GetTree().ChangeSceneToFile("res://Scenes/StageSelect/StageSelect.tscn"); 
+	}
 
 	private void OnClickGameStart()
 	{

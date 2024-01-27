d82d1af4fe77a0783f21d3aab748ed31cbe86ca1
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2024-01-27 21:32:50 +0900

    add EvaluatorTest

--- Scenes/Tester.cs
+++ Scenes/Tester.cs
@@ -1,15 +1,65 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Tester : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		GD.Print("log here");
+
+		Grid testGrid = new Grid();
+		GD.Print("this here");
+
+		for (int i = 0; i < 3; i++)
+		{
+			testGrid.Tiles.Add(new List<Tile>());
+			GD.Print("for index "+i);
+		}
+
+
+		Tile tile1 = new Tile();
+		tile1.East = EdgeType.Connected;
+		tile1.West = EdgeType.DisConnected;
+		tile1.South = EdgeType.DisConnected;
+		tile1.North = EdgeType.DisConnected;
+		tile1.Node = NodeType.NodeType1;
+		testGrid.Tiles[0].Add(tile1);
+		testGrid.Tiles[0].Add(null);
+
+		Tile tile2 = new Tile();
+		tile2.East = EdgeType.Connected;
+		tile2.West = EdgeType.Connected;
+		tile2.South = EdgeType.DisConnected;
+		tile2.North = EdgeType.DisConnected;
+		tile2.Node = NodeType.NodeType1;
+		testGrid.Tiles[1].Add(tile2);
+		Tile tile3 = new Tile();
+		tile3.East = EdgeType.Connected;
+		tile3.West = EdgeType.DisConnected;
+		tile3.South = EdgeType.DisConnected;
+		tile3.North = EdgeType.DisConnected;
+		tile3.Node = NodeType.NodeType2;
+		testGrid.Tiles[1].Add(tile3);
+
+		testGrid.Tiles[2].Add(null);
+		Tile tile4 = new Tile();
+		tile4.East = EdgeType.Connected;
+		tile4.West = EdgeType.DisConnected;
+		tile4.South = EdgeType.DisConnected;
+		tile4.North = EdgeType.Connected;
+		tile4.Node = NodeType.NodeType2;
+		testGrid.Tiles[2].Add(tile4);
+
+		GD.Print("here");
+
+		GridEvaluator myGridEvaluator = GetNode<GridEvaluator>("res://Scripts/View/GridEvaluator.cs");
+		bool isCorrect = myGridEvaluator.EvaluationEdgeCondition(testGrid);
+		GD.Print(isCorrect);
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		
--- Scripts/View/GridEvaluator.cs
+++ Scripts/View/GridEvaluator.cs
@@ -10,11 +10,11 @@ public partial class GridEvaluator : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Grid sampleGrid = new Grid();
+		//Grid sampleGrid = new Grid();
 		//GD.Print("is this working?");
 	}
 	
-	bool EvaluationEdgeCondition(Grid grid) //엣지체킹함수
+	public bool EvaluationEdgeCondition(Grid grid) //엣지체킹함수
 	{
 		for(int i = 0; i<grid.Tiles.Count; i++) 
 		{
@@ -45,60 +45,60 @@ public partial class GridEvaluator : Node
 		return true;
 	}
 
-	int EvaluateScore(Grid grid) //콤보체킹함수
-    {
-        List<List<Vector2I>> comboList = new List<List<Vector2I>>();
+	public int EvaluateScore(Grid grid) //콤보체킹함수
+	{
+		List<List<Vector2I>> comboList = new List<List<Vector2I>>();
 		List<List<bool>> isNodeChecked = new List<List<bool>>(); // 체크 여부
-        
+		
 		int curComboGroup = 0; // 현재 몇 번째 콤보그룹 확인 중인가?
-        comboList.Add(new List<Vector2I>());
+		comboList.Add(new List<Vector2I>());
 
 		bool isFinishedChecking = false;
 
-        for (int i=0; i<grid.Tiles.Count; i++)
+		for (int i=0; i<grid.Tiles.Count; i++)
 		{
-            isNodeChecked.Add(new List<bool>());
+			isNodeChecked.Add(new List<bool>());
 			for(int j=0; j < grid.Tiles[i].Count; j++)
 			{
-                isNodeChecked[i].Add(false);
+				isNodeChecked[i].Add(false);
 			}
 		}
 
 		do
 		{
-            for (int i = 0; i < grid.Tiles.Count; i++)
-            {
-                for (int j = 0; j < grid.Tiles[i].Count; j++)
-                {
-                    if (isNodeChecked[i][j]) continue;
-                    isNodeChecked[i][j] = true;
+			for (int i = 0; i < grid.Tiles.Count; i++)
+			{
+				for (int j = 0; j < grid.Tiles[i].Count; j++)
+				{
+					if (isNodeChecked[i][j]) continue;
+					isNodeChecked[i][j] = true;
 
-                    List<Vector2I> newCombo = new List<Vector2I>();
-                    newCombo = EvaluateEdgeForCombo(grid, new Vector2I(i, j)); // 새로 조사해 온 배열
-                    for (int k = 0; k < newCombo.Count; k++)
-                    {
-                        if (comboList[curComboGroup].Contains(newCombo[k])) { newCombo.Remove(newCombo[k]); } //중복은 지워주고
-                        if (newCombo == null)  //더 이상 새로 콤보 추가할 후보가 없다면..
-                        {
-                            curComboGroup++; // 다음콤보 체킹으로 넘어가기
-                            comboList.Add(new List<Vector2I>()); // 다음콤보 리스트 미리 만들어두기
-                            break;
-                        }
-                        else
-                        {
-                            for (int l = 0; l < newCombo.Count; l++)
-                            {
-                                comboList[curComboGroup].Add(newCombo[l]); //추가된 콤보를 확정 배열로 집어넣기
-                                isNodeChecked[newCombo[l].X][newCombo[l].Y] = true; //체킹 true로 설정
-                            }
+					List<Vector2I> newCombo = new List<Vector2I>();
+					newCombo = EvaluateEdgeForCombo(grid, new Vector2I(i, j)); // 새로 조사해 온 배열
+					for (int k = 0; k < newCombo.Count; k++)
+					{
+						if (comboList[curComboGroup].Contains(newCombo[k])) { newCombo.Remove(newCombo[k]); } //중복은 지워주고
+						if (newCombo == null)  //더 이상 새로 콤보 추가할 후보가 없다면..
+						{
+							curComboGroup++; // 다음콤보 체킹으로 넘어가기
+							comboList.Add(new List<Vector2I>()); // 다음콤보 리스트 미리 만들어두기
+							break;
+						}
+						else
+						{
+							for (int l = 0; l < newCombo.Count; l++)
+							{
+								comboList[curComboGroup].Add(newCombo[l]); //추가된 콤보를 확정 배열로 집어넣기
+								isNodeChecked[newCombo[l].X][newCombo[l].Y] = true; //체킹 true로 설정
+							}
 
-                        }
+						}
 
-                    }
-                }
-            }
+					}
+				}
+			}
 
-            for(int i = 0; i < isNodeChecked.Count; i++)
+			for(int i = 0; i < isNodeChecked.Count; i++)
 			{
 				if (isNodeChecked[i].Contains(false)) break;
 				isFinishedChecking = true;
@@ -109,7 +109,7 @@ public partial class GridEvaluator : Node
 
 		return FinalScore;
 
-    }
+	}
 
 	private List<Vector2I> EvaluateEdgeForCombo(Grid grid, Vector2I nodePos)
 	{
@@ -125,20 +125,20 @@ public partial class GridEvaluator : Node
 			switch (i)
 			{
 				case 0: //상
-                    isCombo[i] = curTile.North == grid.Tiles[ny - 1][nx].South && curTile.Node == grid.Tiles[ny - 1][nx].Node;
-                    break;
+					isCombo[i] = curTile.North == grid.Tiles[ny - 1][nx].South && curTile.Node == grid.Tiles[ny - 1][nx].Node;
+					break;
 
 				case 1: //하
-                    isCombo[i] = curTile.West == grid.Tiles[ny + 1][nx].East && curTile.Node == grid.Tiles[ny + 1][nx].Node;
-                    break;
+					isCombo[i] = curTile.West == grid.Tiles[ny + 1][nx].East && curTile.Node == grid.Tiles[ny + 1][nx].Node;
+					break;
 
 				case 2: //좌
-                    isCombo[i] = curTile.West == grid.Tiles[ny][nx - 1].East && curTile.Node == grid.Tiles[ny][nx - 1].Node;
-                    break;
+					isCombo[i] = curTile.West == grid.Tiles[ny][nx - 1].East && curTile.Node == grid.Tiles[ny][nx - 1].Node;
+					break;
 
 				case 3: //우
-                    isCombo[i] = curTile.West == grid.Tiles[ny][nx + 1].East && curTile.Node == grid.Tiles[ny][nx + 1].Node;
-                    break;
+					isCombo[i] = curTile.West == grid.Tiles[ny][nx + 1].East && curTile.Node == grid.Tiles[ny][nx + 1].Node;
+					break;
 
 				default:
 					break;
@@ -154,7 +154,7 @@ public partial class GridEvaluator : Node
 			}
 		}
 
-        return SurroundingComboTiles;
+		return SurroundingComboTiles;
 	}
 	
 	/*

39f5ba4aeae4029e1470f8dbccd8571d7824296f
HLee <lhslhg@gmail.com>
2024-01-28 01:52:58 +0900

    change input event into unhandled input

--- Scenes/Input/Input.cs
+++ Scenes/Input/Input.cs
@@ -30,7 +30,7 @@ public partial class Input : Node
 		GD.Print(viewer.GetTilePos());*/
 	}
 
-	public override void _Input(InputEvent @event)
+	public override void _UnhandledInput(InputEvent @event)
 	{
 		if (@event is InputEventMouseButton eventMouseButton)
 		{

786f79cb379c4f7e0553b221fda83d272ba3a200
HLee <lhslhg@gmail.com>
2024-01-28 07:48:17 +0900

    Add scene manager

--- Scripts/Controller/SceneManager.cs
+++ Scripts/Controller/SceneManager.cs
@@ -22,28 +22,20 @@ public partial class SceneManager : Node
 		GetTree().ChangeSceneToPacked(MainScene);
 		
 	}
-	// Called when the node enters the scene tree for the first time.
+
+	public void Replay()
+	{
+		GetTree().ChangeSceneToPacked(MainScene);
+		
+	}
+
+	public void StageSelect()
+	{
+		GetTree().ChangeSceneToPacked(StageSelectScene);
+		
+	}
 	public override void _Ready()
 	{
 		_MapPath = MapPaths[0];
-		var button = new Button();
-		button.Text = "Game Start";
-		button.Pressed += StartToStageSelect;
-		AddChild(button);
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-	public void StartToStageSelect()
-	{
-		GetTree().ChangeSceneToFile("res://Scenes/StageSelect/StageSelect.tscn"); 
-	}
-
-	private void OnClickGameStart()
-	{
-		GD.Print("button click");
 	}
 }

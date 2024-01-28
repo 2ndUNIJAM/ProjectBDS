d7d2aa6592a8c09c1c949a7f70e540eff1e4c747
HLee <lhslhg@gmail.com>
2024-01-28 13:00:25 +0900

    Add money fountain effect

--- Scenes/Main/ScoreLabel.cs
+++ Scenes/Main/ScoreLabel.cs
@@ -7,10 +7,16 @@ public partial class ScoreLabel : Label
 	double speed = 0.1f;
 	int TargetScore;
 	double CurrentScore = 0;
+	GpuParticles2D particle;
 
 	public void SetTargetScore(int targetScore)
 	{
 		TargetScore = targetScore;
+	}
+	
+	public override void _Ready()
+	{
+		particle = GetNode<GpuParticles2D>("vfx_starGetting");
 	}
 
 	public override void _Process(double delta)
@@ -22,6 +28,10 @@ public partial class ScoreLabel : Label
 		else if (CurrentScore < TargetScore)
 		{
 			CurrentScore += delta * speed;
+			if(!particle.Emitting)
+			{
+				particle.Restart();
+			}
 		}
 		Text = $"{(int)CurrentScore}억원";
 	}

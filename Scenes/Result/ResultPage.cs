a93bc47546d183c258a957c09809450b0f336970
Yongmalyang <37902200+Yongmalyang@users.noreply.github.com>
2024-01-28 09:52:54 +0900

    submit button

--- Scenes/Result/ResultPage.cs
+++ Scenes/Result/ResultPage.cs
@@ -10,9 +10,27 @@ public partial class ResultPage: Control
     [Export]
     Texture2D[] Stamps = new Texture2D[4];
 
+    
+
     public override void _Ready()
     {
         SetResult(3);
+
+        Button submitButton = GetNode<Button>("../HUD/Submit");
+        submitButton.Text = "SUBMIT";
+        submitButton.Pressed += GameOver;
+        //AddChild(submitButton);
+    }
+
+    void GameOver()
+    {
+        GD.Print("over here");
+        Control myInventory = GetNode<Control>("../HUD/Inventory");
+        myInventory.Visible = false;
+        Control myResult = GetNode<Control>("../HUD/Result");
+        myResult.Visible = true;
+
+        SetResult(0);
     }
 
     public void SetResult(int grade)
